7b7bcb5aa8d071c4df266737555578446568e400
unknown <you@example.com>
2022-05-10 21:57:40 +0400

    Задание на стеки и очереди (только инициализация и ввод стека)

---
+++ stack_queue/2.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+struct stack{
+	int inf;
+	stack * next;
+};
+int pop(stack*& h) { 
+	int i = h -> inf;
+	stack * r = h;
+	h = h -> next;
+	delete r;
+	return i;
+
+}
+void push(stack *&h, int x){
+    stack *r = new stack;
+    r->inf = x;
+    r->next = h;
+    h = r;
+}
+
+void reverse(stack*& h) { 
+	stack * head1 = NULL;
+	while (h) {
+		push(head1, pop(h));
+	}
+	h = head1; 
+	
+}
+
+int main() {
+	int n, x; cin >> n;
+	stack* h = NULL;
+	for (int i = 0; i < n; i++) {
+		cin >> x;
+		push(h, x);
+	}
+
+}
