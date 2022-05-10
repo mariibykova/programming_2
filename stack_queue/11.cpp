cdc10dd8825496d8b5568ba837a1bf3c33fb35e2
unknown <you@example.com>
2022-05-10 22:20:19 +0400

    Инициализация и ввод очереди

---
+++ stack_queue/11.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+struct queue {
+    int inf;
+    queue *next;
+};
+void push (queue *&h,queue *&t, int x){ 
+    queue *r = new queue; 
+    r->inf = x;
+    r->next = NULL; 
+    if (!h && !t) h = t = r; 
+    else {
+        t->next = r; 
+        t = r;
+    }
+}
+int pop (queue *&h, queue *&t){ 
+    queue *r = h; 
+    int i = h->inf; 
+    h = h->next; 
+    if (!h) t = NULL;
+    delete r; 
+    return i;
+}
+int main(){
+    int n, x; cin >> n;
+    queue *h = NULL; queue *t = NULL;
+    for(int i = 0; i < n; i++){
+        cin >> x;
+        push(h, t, x);
+    }
+   
+    return 0;
+}
+
+
