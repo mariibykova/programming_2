82cc573f806ff34649e0bd163d1d40dd150661fb
unknown <you@example.com>
2022-05-10 22:22:29 +0400

    Добавлен вывод сначала отрицателых, потом положительных элементов очереди

--- stack_queue/11.cpp
+++ stack_queue/11.cpp
@@ -29,7 +29,17 @@ int main(){
         cin >> x;
         push(h, t, x);
     }
-   
+    queue *h1 = NULL; queue *t1 = NULL;
+    queue *h_copy = NULL; queue *t_copy = NULL;
+    while(h){
+        int i=pop(h, t);
+        if (i<0) push(h1, t1, i);
+        if (i>=0) push(h_copy, t_copy, i);
+    }
+    while(h_copy){
+        push(h1, t1, pop(h_copy, t_copy));
+    }
+    while(h1) cout << pop(h1, t1) << " ";
     return 0;
 }
 
